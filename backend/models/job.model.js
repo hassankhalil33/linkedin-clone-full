937953b4c2f776c51ae2d836f1ef3d2cf5993435
hassankhalil33 <Hassan.Khalil.33@Hotmail.com>
2022-10-15 14:03:42 +0300

    Backend <> Added Job Model

---
+++ backend/models/job.model.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+
+const jobSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: 'name is required',
+    trim: true
+  },
+
+  description: {
+    type: String,
+    trim: true
+  },
+
+  company: {
+    type: String
+  },
+
+  applicants: {
+    type: Array
+  },
+
+  worker: {
+    type: String
+  },
+
+  timestamp: {
+    
+  }
+})
+
+const Job = mongoose.model('Job', jobSchema);
+
+module.exports = Job;
