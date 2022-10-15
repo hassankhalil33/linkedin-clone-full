47a88ccba4f1a407f773fe419fefdaadd14599ba
hassankhalil33 <Hassan.Khalil.33@Hotmail.com>
2022-10-15 17:17:01 +0300

    Backend <> Updated Middleware in Company Routes

--- backend/routes/company.routes.js
+++ backend/routes/company.routes.js
@@ -1,9 +1,9 @@
 const {Router} = require('express');
 const {} = require('../controllers/company.controller');
-const userMiddleware = require('../middlewares/company.middleware');
+const companyMiddleware = require('../middlewares/company.middleware');
 const router = Router();
 
-router.get('/', userMiddleware, getUser);
+router.get('/', companyMiddleware, getUser);
 
 
 module.exports = router;
