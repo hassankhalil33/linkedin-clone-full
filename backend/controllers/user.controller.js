84e0a4b9dfd4c521f2548c2a7101b5f267affd0a
hassankhalil33 <Hassan.Khalil.33@Hotmail.com>
2022-10-15 16:44:16 +0300

    Backend <> Created Follow Company API

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -2,23 +2,45 @@ const User = require("../models/user.model");
 const Company = require("../models/company.model");
 const Job = require("../models/job.model");
 
-const getAllCompanies = async (req, res)=>{
+const getAllCompanies = async (req, res) => {
   const companies = await Company.find().lean();
   res.json(companies)
 }
 
-const getUser = async (req, res)=>{
+const getUser = async (req, res) => {
   const user = req.user;
   res.json(user)
 }
 
-const getAllJobs = async (req, res)=>{
+const getAllJobs = async (req, res) => {
   const jobs = await Job.find().lean();
   res.json(jobs)
+}
+
+const followCompany = async (req, res) => {
+  const companyId = req.body.company_id;
+  const user = await User.findOne(req.user.id).lean();
+  await User.updateOne(req.user.id, {following: [...user.following, companyId]})
+
+  res.status(200).json({message: "success"})
+}
+
+const unfollowCompany = async (req, res) => {
+  const companyId = req.body.company_id;
+  const user = await User.findOne(req.user.id).lean();
+  const data = user.following.filter((id) => {
+    return id != companyId
+  })
+
+  await User.updateOne(req.user.id, {following: data})
+
+  res.status(200).json({message: "success"})
 }
 
 module.exports = {
   getAllCompanies,
   getUser,
-  getAllJobs
+  getAllJobs,
+  followCompany,
+  unfollowCompany
 }
--- backend/routes/users.routes.js
+++ backend/routes/users.routes.js
@@ -1,11 +1,13 @@
 const {Router} = require('express');
-const {getAllCompanies, getUser} = require('../controllers/user.controller');
+const {getAllCompanies, getUser, getAllJobs, followCompany, unfollowCompany} = require('../controllers/user.controller');
 const userMiddleware = require('../middlewares/user.middleware');
 const router = Router();
 
 router.get('/companies', userMiddleware, getAllCompanies);
 router.get('/', userMiddleware, getUser);
-router.get('/jobs', userMiddleware, getUser);
+router.get('/jobs', userMiddleware, getAllJobs);
+router.post('/follow', userMiddleware, followCompany);
+router.post('/unfollow', userMiddleware, unfollowCompany);
 
 
 module.exports = router;
