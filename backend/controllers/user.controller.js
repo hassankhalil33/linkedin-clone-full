75ff56a1c0409cfb71ddfc5227c0669404fc04e1
hassankhalil33 <Hassan.Khalil.33@Hotmail.com>
2022-10-15 15:27:32 +0300

    Backend <> Added All users API

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,5 +1,11 @@
+const User = require("../models/user.model");
 
 const getAllUsers = async (req, res)=>{
+  const users = await User.find().lean();
+  res.json({users: users})
+}
+
+const getUser = async (req, res)=>{
   const user = req.user;
   res.json({user: user})
 }
