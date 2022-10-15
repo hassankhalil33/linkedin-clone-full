dabd11c0409e780b0ae0925c7926b022b7b83b8c
hassankhalil33 <Hassan.Khalil.33@Hotmail.com>
2022-10-15 14:24:40 +0300

    Backend <> Added login to Auth Controller

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -21,6 +21,21 @@ const signup = async (req, res) => {
   }
 }
 
+const login = async (req, res) => {
+  const {email, password} = req.body;
+  const user = await User.findOne({email}).select("+password");
+
+  if(!user) return res.status(404).json({message: "Invalid Credentials"});
+
+  const isMatch = bcrypt.compare(password, user.password);
+  if(!isMatch) return res.status(404).json({message: "Invalid Credentials"});
+
+  const token = jwt.sign({email: user.email}, process.env.JWT_SECRET_KEY, {
+    expiresIn: '3h'
+  });
+  res.status(200).json(token)
+}
+
 module.exports = {
   login,
   signup
