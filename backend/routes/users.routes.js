9ac38abf247a55c20b405fe16991023f2d9767e7
hassankhalil33 <Hassan.Khalil.33@Hotmail.com>
2022-10-16 17:42:26 +0300

    Backend <> Added New User Routes

--- backend/routes/users.routes.js
+++ backend/routes/users.routes.js
@@ -5,16 +5,27 @@ const {
   getAllJobs,
   followCompany,
   unfollowCompany,
-  retrieveNotifications} = require('../controllers/user.controller');
+  retrieveNotifications,
+  updateUser,
+  applyJob,
+  revokeApplyJob,
+  applyJobEasy
+} = require('../controllers/user.controller');
 const userMiddleware = require('../middlewares/user.middleware');
 const router = Router();
 
 router.get('/', userMiddleware, getUser);
+router.put('/', userMiddleware, updateUser);
+
 router.get('/companies', userMiddleware, getAllCompanies);
 router.get('/jobs', userMiddleware, getAllJobs);
 router.post('/follow', userMiddleware, followCompany);
 router.post('/unfollow', userMiddleware, unfollowCompany);
 router.get('/notifications', userMiddleware, retrieveNotifications);
 
+router.post('/apply', userMiddleware, applyJob);
+router.delete('/apply', userMiddleware, revokeApplyJob);
+
+router.post('/apply_easy', userMiddleware, applyJobEasy);
 
 module.exports = router;
