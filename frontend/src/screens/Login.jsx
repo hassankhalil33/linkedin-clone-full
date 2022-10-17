9556d7508984930da5243513dd87e995cc23ee25
hassankhalil33 <Hassan.Khalil.33@Hotmail.com>
2022-10-17 19:54:59 +0300

    Linking <> Removed Hooks, Added Axios Instant, Linked Login

--- frontend/src/screens/Login.jsx
+++ frontend/src/screens/Login.jsx
@@ -1,9 +1,23 @@
 import React from "react";
 import LoginForm from "../components/LoginForm";
+import axios from "../api/axios";
 
 function Login() {
-  const login = () => {
-    console.log("logged in");
+  const batata = {
+    email: "asdas@gmail.com",
+    password: "Codi33",
+    type: "company"
+  }
+
+  const login = async () => {
+    try {
+      const response = await axios.post("/login", batata);
+      console.log(response.data);
+
+      console.log("logged in");
+    } catch (err) {
+      console.log(err.response.data);
+    }
   }
 
   return (
